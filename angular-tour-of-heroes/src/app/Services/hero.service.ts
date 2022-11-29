6712c991427dec9c96c714722238856184e1cb0a
itisanilk <76086864+itisanilk@users.noreply.github.com>
2022-11-29 17:48:52 +0530

    search url change

--- angular-tour-of-heroes/src/app/Services/hero.service.ts
+++ angular-tour-of-heroes/src/app/Services/hero.service.ts
@@ -62,7 +62,7 @@ deleteHero(Id: number) : Observable<Hero> {
 searchHeroes(term: string) : Observable<Hero[]>{
   if(!term.trim())
    { return of([]); }
-  const url = `http://localhost:3000/api/heroes/${term}/anystring`
+  const url = `${this.heroUrl}/${term}/anystring`
   return this.httpClient.get<Hero[]>(url).pipe(tap(x=>x.length ? 
                this.log(`found heroes matching "${term}"`) :
                this.log(`No heroes matching "${term}"`)),
